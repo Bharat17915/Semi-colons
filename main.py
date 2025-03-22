52ddba407c2a07f492f045823d6985ff29a1e72a
Kaizoku Bharat <bharattatti753@gmail.com>
2025-03-22 10:45:58 +0530

    Added sheet name as parameter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, Form
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
